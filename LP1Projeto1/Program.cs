cf96f25512711a3ca0f67aaa9a17945d14ee39a6
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-04-10 18:38:26 +0100

    Basic structure

--- LP1Projeto1/Program.cs
+++ LP1Projeto1/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 
+//B->BOTÃO L->LAMPADA
+//1B-->1L  2B-->1L,2L   3B-->2L,3L
+//Maximo de 6 jogadas.
 namespace LP1Projeto1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Starting Game!");
+            int NumbMoves=0;
+            
+            bool LAMP1=false;
+            bool LAMP2=false;
+            bool LAMP3=false;
+
+            
+            while (NumbMoves<7)
+            {
+            Console.WriteLine("Clicks on buttons: "+NumbMoves);
+            Console.WriteLine("Which button will you press? B1,B2 or B3.");
+            string ChosenBUT=Console.ReadLine();
+
+            if (ChosenBUT=="B1" || ChosenBUT=="b1"){
+                
+                
+
+
+            }
+            else if (ChosenBUT=="B2" || ChosenBUT=="b2"){
+                
+                
+
+
+            }
+            else if (ChosenBUT=="B3" || ChosenBUT=="b3"){
+                
+                
+
+
+            }
+            else{
+                Console.WriteLine("No valid input");
+            }
+
+            }
+            
         }
     }
 }
